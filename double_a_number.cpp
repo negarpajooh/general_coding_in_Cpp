9dfca30df6f31cef353673eb9f4b25313332c804
Mojtaba Hasanlu <44930640+negarpajooh@users.noreply.github.com>
2026-09-18 09:45:12 +0800

    Add files via upload

---
+++ double_a_number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{ double x{};
+  double y{0};
+   std::cout <<"plz enter your number";
+   std::cin >> x;
+   y=2*x;
+   std::cout<<"output : "<<y;
+   return 0;
+}
