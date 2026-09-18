0eece0e513e00919f60f6d84c940f5bffb2f701d
Mojtaba Hasanlu <44930640+negarpajooh@users.noreply.github.com>
2026-09-18 21:50:48 +0800

    Add files via upload

---
+++ product_sum_two_numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int Program1 (int x){
+return x*x;
+}
+
+int Program2 (int x){
+return x+x;
+}
+
+int main(){
+std::cout<<Program1 (5)<<"\n";
+std::cout<<Program2 (5)<<"\n";
+ return 0;
+}
+
+
+
