bec235f96467f38431c9096c06dce97dc1c07b01
Mojtaba Hasanlu <44930640+negarpajooh@users.noreply.github.com>
2026-09-18 21:37:40 +0800

    Add files via upload

---
+++ print_qoute.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+void Quote()
+{   std::string Abraham_Lincoln{
+        "Give me six hours to chop down "
+        "a tree and I will spend the first four sharpening the axe."};
+    std::cout << Abraham_Lincoln << '\n';}
+int main()
+{   Quote();
+    return 0;
+}
+
+
