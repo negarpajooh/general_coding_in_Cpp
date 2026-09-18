bdac68bf3e48b1beb96713af0e8ae994e85fa840
Mojtaba Hasanlu <44930640+negarpajooh@users.noreply.github.com>
2026-09-18 23:00:51 +0800

    Add files via upload

---
+++ size_int_double.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    //short s;
+    //int i;
+    //long l;
+    //long long ll;
+    std::cout << sizeof(int)<<" byte for integer"<<"\n";
+    std::cout << sizeof(double)<<" byte for double number"<<"\n";
+    std::cout<< sizeof(float)<<" byte for float number";
+    return 0;
+}
