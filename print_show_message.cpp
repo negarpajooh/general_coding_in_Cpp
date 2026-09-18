fe698f8a209461838c2edd0c27d17ee1ce04e489
Mojtaba Hasanlu <44930640+negarpajooh@users.noreply.github.com>
2026-09-18 09:51:51 +0800

    Add files via upload

---
+++ print_show_message.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void showingmesseage(){
+std::cout<<"welcome to C++"<<"\n";
+std::cout<<"My name is Mojtaba Hasanlu"<<"\n";
+std::cout<<"-------------------------------------------------------------------------";
+}
+
+int main(){
+   showingmesseage();
+   return 0;
+}
+
