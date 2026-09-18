ff2bf645c2331f414da79d3e4c488403c08debdd
Mojtaba Hasanlu <44930640+negarpajooh@users.noreply.github.com>
2026-09-18 09:30:42 +0800

    Add files via upload

---
+++ Simplest_print_personal_infomration.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int age=30;
+     std:: cout << "The Age is = " << age;
+     std:: cout << " \n";
+    int width{20};
+     std::cout << "Wdith is = " <<width;
+     return 0;
+}
