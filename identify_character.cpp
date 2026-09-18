453a71ae53f34a70f877aa9181f924d503120596
Mojtaba Hasanlu <44930640+negarpajooh@users.noreply.github.com>
2026-09-18 21:36:36 +0800

    Add files via upload

---
+++ identify_character.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+int main(){
+std::string str{};
+char char1{};
+std:: cout<<"enter your character \n";
+std::cin>> str;
+std::cout<< "your full word: "<<str<<"\n";
+std::cout<< "first character : " <<str[0]<<"\n";
+return 0;
+}
